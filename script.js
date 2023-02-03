1ab9f5bb9baff773dfef9cbb66da6d10f8e6897d
Marcos Tadeu dias da Silva <104788603+marcosmg3@users.noreply.github.com>
2023-02-03 11:19:36 -0300

    lista

---
+++ script.js
@@ -0,0 +1,79 @@
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
+
+function addTarefa() {
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
+
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
+
+    let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+        
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+            <span class="material-symbols-outlined">
+                circle
+            </span>
+
+        </div>
+
+        
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`;
+
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
+
+    //ZERAR OS CAMPINHOS
+    input.value = "";
+    input.focus();
+  }
+}
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove(`("<img src="check_circle_FILL0_wght400_GRAD0_opsz48.png" alt="" srcset="">
+    ")`);
+    icone.classList.add("mdi-check-circle");
+
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("add");
+  }
+}
+
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
